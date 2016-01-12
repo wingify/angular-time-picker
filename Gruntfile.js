8b355ba9daae079dad57ec995aaf4995065f2731
Prayag Verma <prayag.verma@gmail.com>
2016-01-12 23:41:19 +0530

    chore(build): make grunt-htmljs functional

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,12 @@ module.exports = function (grunt) {
             main: {
                 src: ['src/*.html'],
                 dest: 'src/templates.js'
+            },
+            options: {
+                module:'html2js',
+                rename: function (moduleName) {
+                    return '/' + moduleName;
+                }
             }
         },
         karma: {
@@ -30,7 +36,7 @@ module.exports = function (grunt) {
         uglify: {
             js: {
                 files: {
-                    'dist/angular-time-picker.min.js': ['src/angular-time-picker.js']
+                    'dist/angular-time-picker.min.js': ['src/templates.js','src/angular-time-picker.js']
                 }
             }
         },
